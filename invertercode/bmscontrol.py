c5c7a5d7de11fc5172259fb586e7e997516508cc
Daniel McIlroy <sasquachdm@sasquachdesktop.garage>
2018-11-03 18:29:07 -0600

    fixed flow got treading sort of working

--- invertercode/bmscontrol.py
+++ invertercode/bmscontrol.py
@@ -105,34 +105,12 @@ class handle_rs232(threading.Thread):
         self.threadID = threadID
         self.ser = serial.Serial("/dev/ttyUSB1", baudrate=9600, bytesize=8,
                                  parity='E', stopbits=1, timeout=0.1)
-        self.data = {}
         self.key = 0xF007
-        self.value = bms_dict[self.key]
-        self.data.update({self.value['Name']: {'Current': 0, 'New': False}})
+        self.reply_new = False
         self.serial_io()
-        print self.data['Populated Cell Count']['Current']
-        for self.key, self.value in bms_dict.items():
-            if self.value['Name'] not in self.data:
-                print self.value['Name']
-                self.data.update({self.value['Name']: {'New': False}})
-                if self.value['Bytes'] > 1:
-                    self.data[self.value['Name']].update({'Current': []})
-                    if 'Span' in self.value:
-                        print self.value['Name']
-                        for i in range(0, self.data['Populated Cell Count']
-                                       ['Current']):
-                            print i
-                            self.data[self.value['Name']]['Current'].append(0)
-                else:
-                    self.data[self.value['Name']].update({'Current': 0})
-        print self.data
 
     def run(self):
-        x_string = ''
         while True:
-            t_old = time.time()
-            x_string_start_t = time.time()
-            print self.data
             self.get_data()
 
     def get_data(self):
@@ -154,29 +132,76 @@ class handle_rs232(threading.Thread):
         command = ':0322' + str.format('{:04X}', self.key) + '\n'
         self.ser.write(command)
         self.reply = self.ser.read_until()
+        self.reply_new = True
+
+
+class handle_decode_publish(threading.Thread):
+    def __init__(self, threadID, handle_rs232_t):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.handle_rs232_t = handle_rs232_t
+        self.data = {}
+        self.key = 0xF007
+        self.value = bms_dict[self.key]
+        self.data.update({self.value['Name']: {'Current': 0, 'New': False}})
+
+    def run(self):
+        while not self.handle_rs232_t.reply_new:
+            print 'here'
+            time.sleep(0.01)
+        self.handle_rs232_t.reply_new = False
+        self.reply = self.handle_rs232_t.reply
         self.decode()
         self.publish()
-        #print self.reply
+        print self.data['Populated Cell Count']['Current']
+        for self.key, self.value in bms_dict.items():
+            if self.value['Name'] not in self.data:
+                print self.value['Name']
+                self.data.update({self.value['Name']: {'New': False}})
+                if self.value['Bytes'] > 1:
+                    self.data[self.value['Name']].update({'Current': []})
+                    if 'Span' in self.value:
+                        print self.value['Name']
+                        for i in range(0, self.data['Populated Cell Count']
+                                       ['Current']):
+                            print i
+                            self.data[self.value['Name']]['Current'].append(0)
+                else:
+                    self.data[self.value['Name']].update({'Current': 0})
+        print self.data
+
+        while True:
+            time.sleep(0.01)
+            if self.handle_rs232_t.reply_new:
+                self.reply = self.handle_rs232_t.reply
+                self.handle_rs232_t.reply_new = False
+                self.decode()
+                self.publish()
 
     def decode(self):
         self.reply_data = []
         if re.match(r'^:[A-F0-9]+', self.reply):
             self.reply_len = int(self.reply[1] + self.reply[2], 16) - 3
-            self.reply_command = int(self.reply[5] + self.reply[6]
-                                     + self.reply[7] + self.reply[8], 16)
-            if ((self.value['Bytes'] * self.value['Encode']) == self.reply_len
-                    and (self.key == self.reply_command)):
-                self.valid_data = True
-                for x in range(0, self.reply_len*2, self.value['Encode']*2):
-                    if self.value['Encode'] == 1:
-                        self.reply_data.append(int(self.reply[x+9]
-                                                   + self.reply[x+10], 16))
-                    else:
-                        self.reply_data.append(int(self.reply[x+9]
-                                                   + self.reply[x+10]
-                                                   + self.reply[x+11]
-                                                   + self.reply[x+12], 16))
-                self.Scale()
+            self.key = int(self.reply[5] + self.reply[6]
+                           + self.reply[7] + self.reply[8], 16)
+            if self.key in bms_dict:
+                self.value = bms_dict[self.key]
+                if ((self.value['Bytes'] * self.value['Encode']
+                     ) == self.reply_len):
+                    self.valid_data = True
+                    for x in range(0, self.reply_len*2, self.value['Encode']*2):
+                        if self.value['Encode'] == 1:
+                            self.reply_data.append(int(self.reply[x+9]
+                                                       + self.reply[x+10], 16))
+                        else:
+                            self.reply_data.append(int(self.reply[x+9]
+                                                       + self.reply[x+10]
+                                                       + self.reply[x+11]
+                                                       + self.reply[x+12], 16))
+                    self.Scale()
+                else:
+                    print 'bad value'
+                    self.valid_data = False
             else:
                 print 'bad value'
                 self.valid_data = False
@@ -208,27 +233,44 @@ class handle_rs232(threading.Thread):
                                          ['Current']):
                 for i in range(self.value['Span'][0]-1,
                                self.data['Populated Cell Count']['Current']):
-                    print i-(self.value['Span'][0]-1)
-                    self.data[self.value['Name']]['Current'][i] = (
-                        self.reply_data[i-(self.value['Span'][0]-1)])
+                    if (self.data[self.value['Name']]['Current'][i] != (
+                        self.reply_data[i-(self.value['Span'][0]-1)])):
+                        self.data[self.value['Name']]['New'] = True
+                        self.data[self.value['Name']]['Current'][i] = (
+                            self.reply_data[i-(self.value['Span'][0]-1)])
             else:
                 for i in range(self.value['Span'][0]-1,
                                self.value['Span'][1]):
-                    self.data[self.value['Name']]['Current'][i] = (
-                        self.reply_data[i])
+                    if self.data[self.value['Name']]['Current'][i] != (
+                        self.reply_data[i]):
+                        self.data[self.value['Name']]['New'] = True
+                        self.data[self.value['Name']]['Current'][i] = (
+                            self.reply_data[i])
         elif self.value['Bytes'] > 1:
-            self.data[self.value['Name']]['Current'] = self.reply_data
+            if self.data[self.value['Name']]['Current'] != self.reply_data:
+                self.data[self.value['Name']]['New'] = True
+                self.data[self.value['Name']]['Current'] = self.reply_data
         else:
-            self.data[self.value['Name']]['Current'] = self.reply_data[0]
-        self.data[self.value['Name']]['New'] = True
+            if self.data[self.value['Name']]['Current'] != self.reply_data[0]:
+                self.data[self.value['Name']]['New'] = True
+                self.data[self.value['Name']]['Current'] = self.reply_data[0]
 
     def publish(self):
         for key, value in self.data.items():
             if 'New' in value:
                 if value['New']:
                     value['New'] = False
+                    if key == 'Relays Status':
+                        for k, v in bms_dict[0xF004]['Lookup_Table'].items():
+                            if k & value['Current'][1]:
+                                output = True
+                            else:
+                                output = False
+                            print ('BMS/'+str(v['Name']) + ' = ' + str(output))
+                            client.publish('BMS/'+str(v['Name']),
+                                           str(output), retain=True)
                     if isinstance(value['Current'], list):
-                        print ('BMS/'+str(key))
+                        print ('BMS/'+str(key) + ' = ' + str(value['Current']))
                         client.publish('BMS/'+str(key),
                                        json.dumps(value['Current']),
                                        retain=True)
@@ -247,6 +289,9 @@ threadLock = threading.Lock()
 handle_rs232_t = handle_rs232(1)
 handle_rs232_t.daemon = True
 handle_rs232_t.start()
+handle_decode_publish_t = handle_decode_publish(2, handle_rs232_t)
+handle_decode_publish_t.daemon = True
+handle_decode_publish_t.start()
 # handle_485_inverter_data_t = handle_485_inverter_data(2, handle_485_t)
 # handle_485_inverter_data_t.daemon = True
 # handle_485_inverter_data_t.start()
