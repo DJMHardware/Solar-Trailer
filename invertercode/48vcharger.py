252ea8c985a0b49e23bb651cb981a8bd401d68c6
Daniel McIlroy <djmhardware@gmail.com>
2019-01-31 07:53:42 -0700

    got multi unit commands automated

--- invertercode/48vcharger.py
+++ invertercode/48vcharger.py
@@ -26,27 +26,58 @@ class handle_control(threading.Thread):
     def __init__(self, threadID):
         threading.Thread.__init__(self)
         self.threadID = threadID
+        self.command_list = []
+        self.phy_list = {}
         self.init_handle_uart()
+
+    def command(self, command_name, value=None):
+        command = {'command': command_name,
+                   'value': value,
+                   'complete': {},
+                   'reply': {}}
+        self.command_list.append(command)
+
+    def check_command_reply(self):
+        done = None
+        for v in reversed(self.command_list):
+            for c in v['complete']:
+                if v['complete'][c] is True and (done is True or done is None):
+                    done = True
+                else:
+                    done = False
+            if done is True:
+                value = v
+                self.command_list.remove(v)
+                return value
+
+    def get_command(self):
+        return self.command_list
 
     def init_handle_uart(self):
         print 'init control'
         self.handle_uart_t = {}
         for key, value in config['uarts'].items():
-            handle_uart_t = handle_uart(key, value)
+            if value['phy'] not in self.phy_list:
+                self.phy_list[value['phy']] = True
+            handle_uart_t = handle_uart(self, key, value)
             self.handle_uart_t[key] = handle_uart_t
             self.handle_uart_t[key].daemon = True
             self.handle_uart_t[key].start()
 
     def run(self):
+        print self.phy_list
+        self.command('set_voltage', 24)
         while True:
-            print 'run control'
+            value = self.check_command_reply()
+            print 'run control ' + str(value)
             time.sleep(1)
 
 
-class handle_uart(handle_control):
-    def __init__(self, threadID, value):
+class handle_uart(threading.Thread):
+    def __init__(self, control, threadID, value):
         threading.Thread.__init__(self)
         self.threadID = threadID
+        self.control = control
         self.dev = value['dev']
         self.address = config['address']['control']
         print 'init ' + self.dev
@@ -56,18 +87,29 @@ class handle_uart(handle_control):
 
     def run(self):
         while True:
-            print 'read ' + self.dev
-            self.send_command('set_voltage', 3.30)
-            self.send_command('set_current', 2.30)
-            self.send_command('set_output', 1)
-            self.send_command('set_auto_output', 1)
-            self.send_command('read_actual_voltage')
-            self.send_command('read_actual_current')
+            print self.dev + ' check for command'
+            for v in self.control.command_list:
+                if self.dev not in v['complete']:
+                    v['complete'][self.dev] = False
+                    print (self.dev + ' run command = ' + str(v['command'])
+                           + ', ' + str(v['value']))
+                    v['reply'][self.dev] = self.send_command(v['command'],
+                                                             v['value'])
+                    v['complete'][self.dev] = True
+            time.sleep(1)
+        while True:
+            self.send_command('set_voltage', 24)
+            self.send_command('set_current', .5)
+            self.send_command('set_output', 0)
+            self.send_command('set_auto_output', 0)
             self.send_command('read_set_voltage')
             self.send_command('read_set_current')
             self.send_command('read_output_state')
-            self.send_command('read_actual_output_capacity')
-            time.sleep(1)
+            while True:
+                self.send_command('read_actual_voltage')
+                self.send_command('read_actual_current')
+                self.send_command('read_actual_output_capacity')
+                time.sleep(1)
 
     def send_command(self, command_name, value=None):
         command = config['command'][command_name]
@@ -91,7 +133,7 @@ class handle_uart(handle_control):
                     value = float(value) / command['scale']
             else:
                 value = str(value[0])
-            print 'value = ' + str(value)
+            print self.dev + ' value = ' + str(value)
             return(value)
         else:
             raise Exception('error uart returned bad string = ' + str(reply))
@@ -109,7 +151,7 @@ class handle_uart(handle_control):
             time.sleep(0.01)
         self.busy = True
         self.ser.write(output.encode())
-        print 'sent = ' + str(re.sub('\n|\r', '', output))
+        print self.dev + ' sent = ' + str(re.sub('\n|\r', '', output))
         endofline = False
         timeout = 0
         reply = ''
