c53ff9d02bdb644fe1d3e424d42b47b241932d65
Daniel McIlroy <djmhardware@gmail.com>
2019-02-07 13:55:40 -0700

    fixed more bugs will run both bms and charger now

--- invertercode/bms.py
+++ invertercode/bms.py
@@ -63,23 +63,23 @@ class handle_control(threading.Thread):
 
     def command(self, command_name, value=None):
         c = self.config['command'][command_name]
-        c.update(self.config['command']['Default'])
         c.update({'command': command_name,
-                  'value': value,
-                  'output_string': '',
-                  'value_string': '',
-                  'complete': {},
-                  'reply': {}})
+                  'value': value})
+        c.update(self.config['command']['Default'])
         if 'cmds' in c:
             for cmd in c['cmds']:
                 c['cmd'] = cmd
                 c['span'] = c['spans']['0x{:04X}'.format(cmd)]
-                self.build_command(c)
+                self.command_list.append(self.build_command(c))
         else:
-            self.build_command(c)
-        self.command_list.append(c)
+            self.command_list.append(self.build_command(c))
 
     def build_command(self, c):
+        c = c.copy()
+        c.update({'output_string': '',
+                  'value_string': '',
+                  'complete': {},
+                  'reply': {}})
         if 'send_value_format' in c:
             c['value_string'] = self.human_to_machine(c, c['value'])
         c['output_string'] = (c['cmd_format'].format(c['cmd'])
@@ -94,28 +94,27 @@ class handle_control(threading.Thread):
 
     def extract_values(self, c, value):
         print ('raw value = ' + value)
-        re.sub(c['reply_regex_prefix'], '', value)
+        value = re.sub(r'^' + c['reply_regex_prefix'], '', value.encode())
         print ('reply_regex_prefix = ' + c['reply_regex_prefix'])
         print ('stripped value = ' + value)
         if 'payload' in c:
-            c['reply_regex'] += ('{' + str(c['payload']['encode']
-                                           * c['payload']['bytes']) + '}')
-        values = re.findall(c['reply_regex'], value)
-        print (values)
+            reply_regex_mod = ('{' + str(c['payload']['encode'] * 2) + '}')
+        print ('reply_regex = ' + c['reply_regex'])
+        values = re.findall((c['reply_regex'] + reply_regex_mod), value)
         return self.machine_to_human(c, values)
 
     def compute_reply_regex(self, c):
+        r = c['reply_regex']
         if 'payload' in c:
             p = c['payload']
             p['length'] = ((p['encode'] * 2) * p['bytes'])
-            c['reply_regex_prefix'] = (c['prefix'] + '{:02X}'.format((
+            c['reply_regex_prefix'] = (c['reply_prefix'] + '{:02X}'.format((
                 p['length'] / 2) + 3) + '{:02X}'.format(
                     c['mode'] + 0x40) + '{:02X}'.format(c['cmd']))
-            c['reply_regex'] = ('[0-9A-F]{' + str(p['length']) + '}')
+            r += ('{' + str(p['length']) + '}')
         else:
             c['reply_regex_prefix'] = c['reply_prefix'] + c['cmd']
-        c['reply_regex_string'] = ('^' + c['reply_regex_prefix']
-                                   + c['reply_regex'])
+        c['reply_regex_string'] = ('^' + c['reply_regex_prefix'] + r)
         return c
 
     def human_to_machine(self, c, value):
@@ -131,17 +130,19 @@ class handle_control(threading.Thread):
             return ''
 
     def machine_to_human(self, c, values):
+        if values[0] == 'ok':
+            return values[0]
         if 'Lookup_Table' in c:
             value = int(values[0], c['encoding_base'])
             values = {}
             for i in c['Lookup_Table']:
                 values[i['name']] = bool(value & i['mask'])
             return values
-        if values[0] == 'ok':
-            return values[0]
-        for value in values:
+        for i in range(len(values)):
+            value = values[i]
             if c['range']['scale'] is not 1:
-                value = (float(value) * c['range']['scale'])
+                value = (float(int(value, c['encoding_base']))
+                         * c['range']['scale'])
                 c['reply_value_format'] = (
                     '{:.' + str(self.num_after_point(c['range']['scale']))
                     + 'f}')
@@ -151,7 +152,7 @@ class handle_control(threading.Thread):
                 value = c['range']['min']
             if value > c['range']['max']:
                 value = c['range']['max']
-            value = c['reply_value_format'].format(value)
+            values[i] = c['reply_value_format'].format(value)
         if len(values) > 1:
             return values
         else:
@@ -172,20 +173,22 @@ class handle_control(threading.Thread):
                 else:
                     done = False
             if done is True:
-                if self.config['command'][v['command']]['reply_regex'] != 'ok':
+                if v['reply_regex'] != 'ok':
                     self.publish_to_mqtt(v)
                 else:
                     print ('command ' + v['command'] + '\nreturned ok')
+                print ('complete ' + str(v['complete']))
+                print ('remove ' + str(v['cmd']))
                 self.command_list.remove(v)
 
-    @classmethod
-    def publish_to_mqtt(cls, v):
+    def publish_to_mqtt(self, v):
         reply = []
         for vv in v['reply']:
             reply.append(v['reply'][vv])
         print ('test' + str(reply))
-        cls.client.publish(cls.config['mqtt']['topic'] + '/'
-                           + str(v['command']), json.dumps(reply), retain=True)
+        self.client.publish(self.config['mqtt']['topic'] + '/'
+                            + str(v['command']), json.dumps(reply),
+                            retain=True)
 
     def mqtt_on_message(self, client, userdata, message):
         topic = message.topic.split('/')
@@ -218,14 +221,12 @@ class handle_uart(threading.Thread):
         self.config = control.config
         self.dev = value['dev']
         print ('init ' + self.dev)
-        self.ser = serial.Serial(self.dev, baudrate=4800, bytesize=8,
-                                 parity='N', stopbits=1, timeout=0)
-
-#                                 baudrate=self.config['uart']['baudrate'],
-#                                 bytesize=self.config['uart']['bytesize'],
-#                                 parity=self.config['uart']['parity'],
-#                                 stopbits=self.config['uart']['stopbits'],
-#                                 timeout=self.config['uart']['timeout'])
+        self.ser = serial.Serial(self.dev,
+                                 baudrate=self.config['uart']['baudrate'],
+                                 bytesize=self.config['uart']['bytesize'],
+                                 parity=self.config['uart']['parity'],
+                                 stopbits=self.config['uart']['stopbits'],
+                                 timeout=self.config['uart']['timeout'])
         self.busy = False
 
     def run(self):
@@ -233,37 +234,53 @@ class handle_uart(threading.Thread):
         print 'here'
         while True:
             self.check_command_queue()
-            time.sleep(.01)
+            time.sleep(1)
             if i > 100:
                 print (self.control.command_list)
                 i = 0
 
     def check_command_queue(self):
-            for v in self.control.command_list:
-                if self.dev not in v['complete']:
-                    v['complete'][self.dev] = False
-                    i = 0
-                    while True:
-                        if i == 0:
-                            print (self.dev + ' run command = '
-                                   + str(v['command']) + ', '
-                                   + str(v['value']))
-                        else:
-                            print (self.dev + ' retry command = '
-                                   + str(v['command']) + ', '
-                                   + str(v['value']))
-                        try:
-                            value = (self.send_command(v))
-                            v['reply'][self.dev] = self.control.extract_values(
-                                v, value)
-                        except Exception as e:
-                            i += 1
-                            if i > 10:
-                                raise Exception(str(e))
-                            time.sleep(.01)
-                            continue
-                        break
-                    v['complete'][self.dev] = True
+        for i in range(len(self.control.command_list)):
+            print ('start ' + str(i) + ' = ' + str(
+                self.control.command_list[i]['cmd']) + ' = '),
+            print (self.control.command_list[i]['complete'])
+        for v in self.control.command_list:
+            if self.dev not in v['complete']:
+                v['complete'][self.dev] = False
+                i = 0
+                while True:
+                    if i == 0:
+                        print (self.dev + ' run command = '
+                               + str(v['command']) + ', '
+                               + str(v['value']))
+                    else:
+                        print (self.dev + ' retry command = '
+                               + str(v['command']) + ', '
+                               + str(v['value']))
+                    try:
+                        value = (self.send_command(v))
+                        v['reply'][self.dev] = self.control.extract_values(
+                            v, value)
+                        for i in range(len(self.control.command_list)):
+                            print ('mid' + str(i) + ' = ' + str(
+                                self.control.command_list[i]['cmd']) + ' = '),
+                            print (self.control.command_list[i]['complete'])
+                    except Exception as e:
+                        i += 1
+                        if i > 10:
+                            raise Exception(str(e))
+                        time.sleep(.01)
+                        continue
+                    break
+                for i in range(len(self.control.command_list)):
+                    print ('end 1 ' + str(i) + ' = ' + str(
+                        self.control.command_list[i]['cmd']) + ' = '),
+                    print (self.control.command_list[i]['complete'])
+                v['complete'][self.dev] = True
+                for i in range(len(self.control.command_list)):
+                    print ('end 2 ' + str(i) + ' = ' + str(
+                        self.control.command_list[i]['cmd']) + ' = '),
+                    print (self.control.command_list[i]['complete'])
 
     def send_command(self, c):
         reply = self.uart_IO(c['output_string'], c['suffix'])
@@ -307,14 +324,14 @@ class handle_uart(threading.Thread):
         return(reply)
 
 
-# control_bms = Control('bmscontrol.toml')
-control_test = Control('test.toml')
-control_test.command('set_voltage', 12)
-control_test.command('set_output', 1)
-control_test.command('read_actual_voltage')
-control_test.command('read_actual_working_time')
-# control_bms.command('Relays Status')
-# control_bms.command('Cell Voltages')
+# control_test = Control('test.toml')
+# control_test.command('set_voltage', 12)
+# control_test.command('set_output', 1)
+# control_test.command('read_actual_voltage')
+# control_test.command('read_actual_working_time')
+control_bms = Control('bmscontrol.toml')
+control_bms.command('Relays Status')
+control_bms.command('Cell Voltages')
 
 while True:
     time.sleep(1)
