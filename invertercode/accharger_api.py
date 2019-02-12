10602cc158b74527e2e4d1095084d79519cfec3c
Daniel McIlroy <djmhardware@gmail.com>
2019-02-12 08:07:13 -0700

    cleanup more small bugs

---
+++ invertercode/accharger_api.py
@@ -0,0 +1,74 @@
+import remote_api
+import uart_driver
+import time
+
+
+class AC_Charger_RemoteCommandData(remote_api.RemoteCommandData):
+    def __init__(self, command_name, command_dict, command_callback):
+        super().__init__(command_name, command_dict, command_callback)
+        self._build_command_string()
+        self._compute_reply_regex()
+        self.raw_reply = {}
+        self.reply_buffer = {}
+        self.complete_list = {}
+
+    def start_commmand(self):
+        super().start_commmand()
+        self.complete_list = {}
+        self.reply_buffer = {}
+
+    def check_complete(self, dev):
+        self.complete_list[dev] = True
+        self.complete = None
+        for k in self.complete_list:
+            if (self.complete_list[k] is True
+                    and (self.complete is True or self.complete is None)):
+                self.complete = True
+            else:
+                self.complete = False
+        super().check_complete(dev)
+
+    def _compute_reply(self):
+        if hasattr(self, 'single_uart') and self.single_uart:
+            for k in self.raw_reply:
+                self.reply = self.raw_reply[k]
+                self.dev = k
+        else:
+            self.reply = self.raw_reply
+
+    def _machine_to_human(self, value, dev):
+        reply = super()._machine_to_human(value, dev)
+        return {dev: reply}
+
+
+class AC_Charger_API(remote_api.RemoteAPI):
+    @classmethod
+    def class_init(cls):
+        return super().class_init('accharger.toml',
+                                  AC_Charger_RemoteCommandData)
+
+    def __init__(self, threadID, config_file, remote_command_data_class):
+        super().__init__(threadID, config_file, remote_command_data_class)
+        self.handle_uart_t = uart_driver.Uart_Driver.class_init(self)
+
+    def get_command(self, dev):
+        c = None
+        if (not self.command_queue.empty() and self.current_command is None):
+            self.current_command = self.command_queue.get()
+        if (self.current_command is not None
+                and dev not in self.current_command.complete_list):
+            self.current_command.complete_list[dev] = False
+            c = self.current_command
+        if c is not None and not len(c.complete_list):
+            c.start_commmand()
+        return c
+
+
+control_test = AC_Charger_API.class_init()
+control_test.run_command('set_voltage', 12)
+control_test.run_command('set_output', 1)
+control_test.run_command('read_actual_voltage')
+control_test.run_command('read_actual_working_time')
+
+while True:
+    time.sleep(0.01)
---
+++ invertercode/uart_driver.py
@@ -0,0 +1,99 @@
+import threading
+import serial
+import time
+import re
+
+
+class Uart_Driver(threading.Thread):
+    @classmethod
+    def class_init(cls, caller):
+        print ('init uarts')
+        handle_uart_t = {}
+        for key, value in caller.config['uarts'].items():
+            handle_uart_t[key] = cls(key, value, caller)
+            handle_uart_t[key].daemon = True
+            handle_uart_t[key].start()
+        return handle_uart_t
+
+    def __init__(self, threadID, value, caller):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.caller = caller
+        self.config = caller.config
+        self.dev = value['dev']
+        self.phy = value['phy']
+        print ('init ' + self.dev)
+        self.ser = serial.Serial(self.dev,
+                                 baudrate=self.config['uart']['baudrate'],
+                                 bytesize=self.config['uart']['bytesize'],
+                                 parity=self.config['uart']['parity'],
+                                 stopbits=self.config['uart']['stopbits'],
+                                 timeout=self.config['uart']['timeout'])
+        self.busy = False
+
+    def run(self):
+        i = 0
+        while True:
+            self.check_command_queue()
+            time.sleep(0.01)
+            if i > 100:
+                # print (self.control.command_list)
+                i = 0
+
+    def check_command_queue(self):
+        c = self.caller.get_command(self.dev)
+        if c is not None:
+            i = 0
+            while True:
+                if i == 0:
+                    print (self.dev + ' run command = '
+                           + str(c.command_name) + ', '
+                           + str(c.value))
+                else:
+                    print (self.dev + ' retry command = '
+                           + str(c.command_name) + ', '
+                           + str(c.value))
+                try:
+                    c.extract_values(self.uart_IO(
+                        c.output_string, c.suffix), self.dev)
+                    time.sleep(0.01)
+                except Exception as e:
+                    i += 1
+                    if i > 10:
+                        raise Exception(repr(e))
+                    time.sleep(.01)
+                    continue
+                break
+
+    def uart_IO(self, output, EOL_string):
+        timeout = 0
+        while (self.ser.out_waiting > 0 and self.ser.in_waiting > 0
+               and self.busy):
+            timeout += 1
+            if timeout > 20 and not self.busy:
+                self.ser.reset_output_buffer()
+                self.ser.reset_input_buffer()
+                timeout = 0
+                raise Exception('serial write timeout ' + str(self.dev))
+            time.sleep(0.01)
+        self.busy = True
+        self.ser.write(output.encode())
+        # print (self.dev + ' sent = ' + str(re.sub(EOL_string, '', output)))
+        endofline = False
+        timeout = 0
+        reply = ''
+        while not endofline:
+            if self.ser.in_waiting > 0:
+                r = self.ser.read()
+                reply += str(r, 'utf-8')
+                if re.search(EOL_string, reply):
+                    reply = re.sub(EOL_string, '', reply)
+                    endofline = True
+            else:
+                timeout += 1
+                if timeout > 20:
+                    endofline = True
+                    raise Exception('serial read timeout ' + str(self.dev))
+                time.sleep(0.01)
+        self.busy = False
+        return(reply)
