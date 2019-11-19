fccae12bc4876df4f7b34fda70af77f2c2673af8
Daniel McIlroy <djmhardware@gmail.com>
2019-11-19 08:43:06 -0700

    node red fixes

--- node-red/settings.js
+++ node-red/settings.js
@@ -65,7 +65,7 @@ module.exports = {
     //debugUseColors: true,
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
-    //flowFile: 'flows.json',
+    flowFile: 'flows_inverterpi.json',
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
@@ -81,11 +81,11 @@ module.exports = {
 
     // By default, all user data is stored in the Node-RED install directory. To
     // use a different location, the following property can be used
-    //userDir: '/home/nol/.node-red/',
+    userDir: '/home/pi/Solar-Trailer/node-red/',
 
     // Node-RED scans the `nodes` directory in the install directory to find nodes.
     // The following property can be used to specify an additional directory to scan.
-    //nodesDir: '/home/nol/.node-red/nodes',
+    //nodesDir: '/home/pi/Solar-Trailer/node-red/node_modules',
 
     // By default, the Node-RED UI is available at http://localhost:1880/
     // The following property can be used to specify a different root path.
